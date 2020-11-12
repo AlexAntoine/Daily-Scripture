9175bcbcf5cdadaf34c90128324ae8cfb2ffd7cd
Abundant Life Church <alc@Abundants-MacBook-Pro.local>
2020-11-11 23:08:04 -0500

    still working on this. setHeader not working

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const https = require('https');
+const http = require('https');
 
-const options = {
-	"method": "GET",
-	"hostname": "rapidapi.p.rapidapi.com",
-	"port": null,
-	"path": "/GetVerses?Book=Luke&chapter=5&VerseFrom=1&VerseTo=5",
-	"headers": {
-		"x-rapidapi-host": "ajith-holy-bible.p.rapidapi.com",
-		"x-rapidapi-key": "95273d0f5cmshb4a85e6e7f6ae1fp1a1e51jsn541c0ed65e43",
-		"useQueryString": true
-	}
-};
+const server = http.createServer(function(res, req){
+   
+    res.setHeader('content-type', 'text/plain');
+    res.write('hello');
+    res.end();
+});
 
-const req = https.get(options, function(res){
 
-    console.dir(res.statusCode);
+server.listen(3000, 'localhost', function(){
+
+    console.log('server is running');
 });
