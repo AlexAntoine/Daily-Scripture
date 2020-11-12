90907badd8fa170dded1e33bcfab3a7820465060
Abundant Life Church <alc@Abundants-MacBook-Pro.local>
2020-11-11 23:07:46 -0500

    re-did file

--- app.js
+++ app.js
@@ -1,38 +1,71 @@
 const http = require("https");
+var qs = require("querystring");
 
 
+function sendScripture(){
 
-function getScripture(book, chapter, startVerse, endVerse)
-{	
 
 	const options = {
 		"method": "GET",
-		"hostname": "rapidapi.p.rapidapi.com",
+		"hostname": "ajith-holy-bible.p.rapidapi.com",
 		"port": null,
-		"path": `/GetVerses?Book=${book}&chapter=${chapter}&VerseFrom=${startVerse}&VerseTo=${endVerse}`,
+		"path": "/GetVerses?Book=isaiah&chapter=55&VerseFrom=11&VerseTo=11",
 		"headers": {
-			"x-rapidapi-host": "ajith-holy-bible.p.rapidapi.com",
 			"x-rapidapi-key": "95273d0f5cmshb4a85e6e7f6ae1fp1a1e51jsn541c0ed65e43",
+			"x-rapidapi-host": "ajith-holy-bible.p.rapidapi.com",
 			"useQueryString": true
 		}
 	};
 
 	const req = http.request(options, function (res) {
-		var message = "";
-	
+		
+		let text = "";
+
 		res.on("data", function (chunk) {
-			message = JSON.parse(chunk);
-
+			text += chunk;
 		});
 
-		res.on("end", function(){
+		res.on("end", function () {
 
-			console.log(message.Output)
+			var passage = JSON.parse(text);
+
+			sendMessage(passage.Output);
+
 		});
-	
 	});
 
 	req.end();
 }
 
-getScripture('Luke', 5, 1, 9);
+function sendMessage(passage)
+{
+		var options = {
+			"method": "POST",
+			"hostname": "quick-easy-sms.p.rapidapi.com",
+			"port": null,
+			"path": "/send",
+			"headers": {
+				"x-rapidapi-host": "quick-easy-sms.p.rapidapi.com",
+				"x-rapidapi-key": "95273d0f5cmshb4a85e6e7f6ae1fp1a1e51jsn541c0ed65e43",
+				"content-type": "application/x-www-form-urlencoded",
+				"useQueryString": true
+			}
+		};
+
+		var req = http.request(options, function (res) {
+			var chunks = [];
+
+			res.on("data", function (chunk) {
+				chunks.push(chunk);
+			});
+
+			res.on("end", function () {
+				var body = Buffer.concat(chunks);
+				console.log(body.toString());
+			});
+		});
+
+		req.write(qs.stringify({message: `${passage}`, toNumber: '16178032176'}));
+		req.end();
+}
+
