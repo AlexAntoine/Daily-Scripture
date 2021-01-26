83e23b47bf8e35dd40045c37a43c0907e5cc232c
AlexAntoine <antoinealex1234@gmial.com>
2021-01-26 05:30:25 -0500

    removed something

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("https");
 var qs = require("querystring");
-var digits = require('./index');
+const request = require('request');
 
 
 function getTime()
@@ -55,40 +55,7 @@ function sendScripture()
 	req.end();
 }
 
-function sendMessage(passage)
-{
-		var options = {
-			"method": "POST",
-			"hostname": "quick-easy-sms.p.rapidapi.com",
-			"port": null,
-			"path": "/send",
-			"headers": {
-				"x-rapidapi-host": "quick-easy-sms.p.rapidapi.com",
-				"x-rapidapi-key": "95273d0f5cmshb4a85e6e7f6ae1fp1a1e51jsn541c0ed65e43",
-				"content-type": "application/x-www-form-urlencoded",
-				"useQueryString": true
-			}
-		};
 
-		var req = http.request(options, function (res) {
-			var chunks = [];
-
-			res.on("data", function (chunk) {
-				chunks.push(chunk);
-			});
-
-			res.on("end", function () {
-				var body = Buffer.concat(chunks);
-				console.log(body.toString());
-			});
-		});
-
-		req.write(qs.stringify({message: `${passage}`, toNumber: digits.arrayOfNumbers[0]}));
-		req.end();
-}
-
-
-sendScripture();
 
 
 //two apis are being used, one to get the scripture and the other to send the scripture vi SMS
